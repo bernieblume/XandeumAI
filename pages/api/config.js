716657c38e9088343ec8a2a4c84462d4dc3096a2
Bernie Blume <bernie.blume@antsle.com>
2025-07-13 02:42:37 -0700

    Switch to CommonJS exports for Vercel compatibility

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,5 +1,5 @@
 // pages/api/config.js
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
